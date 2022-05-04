0b41ec8b8a538eb373ebca3fcf53068b1d963d6e
Gaurang Gupta <69477509+gaurang2200@users.noreply.github.com>
2022-05-04 11:14:26 +0530

    Delete spring-drools directory

--- spring-drools/src/main/java/com/microservices/springdrools/model/CustomerType.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.springdrools.model;
-
-public enum CustomerType {
-    LOYAL, NEW, DISSATISFIED;
-
-    public String getValue() {
-        return this.toString();
-    }
-}
